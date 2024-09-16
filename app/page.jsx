df7228016637de83dead977cfe53a00db9b3555e
osborn <osbornnyakaru44@gmail.com>
2024-09-16 15:45:16 +0300

    edited text on app/page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -6,21 +6,21 @@ const Home = () => {
         <h1 className="head_text text-center">
             Discover & Share    
             <br className="max-md:hidden" />
-            <span className="text-primary-orange text-center"> AI-Powered Prompts </span>
+            <span className="text-primary-orange text-center">Prompts for Generative AI </span>
         </h1>
         <p className="desc text-center">
             <span className="text-primary-orange text-center font-bold">
                 Arifa </span> 
                  is an open-source <span className="text-primary-orange text-center font-bold">
-                 AI prompt-sharing tool
-            </span> in the modern world used to discover, create, and share creative prompts. <br /> 
+                  prompt-sharing tool
+            </span> in the modern world used to discover, create, and share creative prompts for generative <AI></AI>. <br /> 
             <span className="text-primary-orange text-center text-sm font-bold">
-                Useful prompts for generative AI like chatGPT and Gemini.
+                Useful prompts for generative AI like chatGPT and Gemini. 
             </span>  
         </p>
         <Feed />
     </section>  
-    )
-}
+    );
+};
 
 export default Home;
