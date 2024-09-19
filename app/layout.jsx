4858e7df6dd4217f7c449f84b5ecaef18716dc6f
osborn <osbornnyakaru44@gmail.com>
2024-09-19 15:42:13 +0300

    tweaks on texture

--- app/layout.jsx
+++ app/layout.jsx
@@ -23,7 +23,7 @@ const RootLayout = ({ children }) => {
     <html lang='en'>
         <body className={cn('font-sans', fontSans.variable)}>
           <Provider>     
-            <div className='main absolute top-0 z-[-2] h-full w-full bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]'>         
+            <div className='main absolute top-0 z-[-2] h-full w-full bg-[#000000] bg-[radial-gradient(#808080_1px,#00091d_1px)] bg-[size:20px_20px]'>         
                 <div className='gradient'/>
             </div>
             <ThemeProvider
